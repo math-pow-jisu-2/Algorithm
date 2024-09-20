1c2d1d433a87b10bfda9ff4ccfde1e4a65910978
jisu <dlawotn3@naver.com>
2024-09-20 19:40:37 +0900

    set으로 수정

--- ji-soo708/boj-1987.py
+++ ji-soo708/boj-1987.py
@@ -7,7 +7,7 @@ input = sys.stdin.readline
 r,c = map(int, input().split())
 keyboard = [list(input().rstrip()) for _ in range(r)]
 dy,dx = [-1,1,0,0],[0,0,-1,1]
-key = [keyboard[0][0]]
+key = set(keyboard[0][0])
 ans = 0
 
 def dfs(y,x,cnt):
@@ -16,7 +16,7 @@ def dfs(y,x,cnt):
     for d in range(4):
         ny,nx = y+dy[d],x+dx[d]
         if 0<=ny<r and 0<=nx<c and not keyboard[ny][nx] in key:
-            key.append(keyboard[ny][nx])
+            key.add(keyboard[ny][nx])
             dfs(ny,nx,cnt+1)
             key.remove(keyboard[ny][nx])
 
